4c42c3362d177a17d71068068610e94002d25aeb
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-21 18:23:49 +0800

    Course and Section

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -8,3 +8,4 @@ print("\033[1;97mOOP Concept of Simple App Calculator".center(97))
 print("\033[0;36m*" * 90)
 
 print("\033[1;32m\nHello! Your Programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
