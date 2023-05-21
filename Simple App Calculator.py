b36c4c2c9205d8bcdb23d15e579dae6ad33d12a9
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-21 18:21:12 +0800

    Title

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -1,3 +1,8 @@
 #Alexza Jean R. Catanoy
 #BSCPE 1-4
 #OOP Concept of Simple App Calculator
+
+#Title
+print("\033[0;36m*" * 90)
+print("\033[1;97mOOP Concept of Simple App Calculator".center(97))
+print("\033[0;36m*" * 90)
