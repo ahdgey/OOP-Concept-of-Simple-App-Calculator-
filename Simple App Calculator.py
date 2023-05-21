488fdb7500ce2924d353810208210a65a944e160
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-21 18:22:45 +0800

    Programmers name

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -6,3 +6,5 @@
 print("\033[0;36m*" * 90)
 print("\033[1;97mOOP Concept of Simple App Calculator".center(97))
 print("\033[0;36m*" * 90)
+
+print("\033[1;32m\nHello! Your Programmers name is Alexza Jean.")
