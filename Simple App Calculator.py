0498809adbe6137735e2ed8a0fa3774eed2fee4a
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-21 20:48:31 +0800

    Design

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -23,11 +23,11 @@ class Calculator:
     def run(self):
         try:
             #Let the user pick among the four math operations
-            math_operation = input("Hi my friend! Kindly pick one math opeartion among these four (+, -, *, /): ")
+            math_operation = input("\033[0;31m\nHi my friend! Kindly pick one math opeartion among these four (+, -, *, /): \033[1;37m")
 
             #Let the user type in two numbers
-            num1 = float(input("Kindly type in the first number you like: "))
-            num2 = float(input("Kindly type in the second number you like: "))
+            num1 = float(input("\033[0;33m\nKindly type in the first number you like: \033[1;37m"))
+            num2 = float(input("\033[0;33m\nKindly type in the second number you like: \033[1;37m"))
 
             #Using the math operation that the user pick, execute the calculation
 
@@ -51,10 +51,11 @@ class Calculator:
                 raise ValueError("The operation you pick is not in the option.")
 
             #Show the output of the program
-            print("Output: ", output)
+            print("\033[0;35m\nOutput: \033[1;37m", output)
 
+            print("\033[0;35m~" * 90)
             #Ask the user if they want to try again for another operation and numbers
-            option = input("Hi my friend! Would you like to try another operation and number? (yes/no): ")
+            option = input("\033[0;34m\nHi my friend! Would you like to try another operation and number? (yes/no): \033[1;37m")
             
             #If the user said yes, just repeat the whole process
             if option.lower() == "yes":
@@ -62,7 +63,7 @@ class Calculator:
 
             #If the user said no, print "Thank you for using this simple app calculator I made." and then end program
             elif option.lower() == "no":
-                print("Thank you for using this simple app calculator I made.")
+                print("\033[0;30m\nThank you for using this simple app calculator I made.")
 
             else:
                 raise ValueError("The option you choose is not valid.")
