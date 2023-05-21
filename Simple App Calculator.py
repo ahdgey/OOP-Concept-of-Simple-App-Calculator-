da06395ec0257827a9b6f6b090b6ace9bfb4251d
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-21 18:57:37 +0800

    Pseudocode

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -10,3 +10,16 @@ print("\033[0;36m*" * 90)
 print("\033[1;32m\nHello! Your Programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4")
 print("-" * 90)
+
+class Calculator:
+
+    #Constructor
+    #Let the user pick among the four math operations
+    #Let the user type in two numbers
+    #Using the math operation that the user pick, execute the calculation
+    #Execute the calculation
+    #Show the output of the program
+    #Ask the user if they want to try again for another operation and numbers
+    #If the user said yes, just repeat the whole process
+    #If the user said no, print "Thank you for using this simple app calculator I made." and then end program
+    #Manage exceptions and apply the run method continuously
